697c329fa54f385e7cda6ea679cf977c968ab3d8
alhwyn <alhwyndwyne0411@gmail.com>
2025-02-28 01:02:25 -0800

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "What did you do last week?",
@@ -21,6 +23,8 @@ export default function RootLayout({
       <head />
       <body>
         {children}
+        <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import supabase from "../Supabaseclient";
-import { Analytics } from "@vercel/analytics/react"
+
 
 export default function Home() {
   const [user, setUser] = useState(null);
